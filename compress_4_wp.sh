ca8e0619367b861b72573f004f23592d4a10da81
g-battaglia <battaglia.giacomo@icloud.com>
2025-03-17 07:49:02 +0100

    Script 4 WP -> Fix

--- compress_4_wp.sh
+++ compress_4_wp.sh
@@ -3,6 +3,6 @@
 OUTPUT_FILE="AstrologerWP.zip"
 
 # Crea l'archivio escludendo i file e le cartelle nascoste
-zip -r "$OUTPUT_FILE" . -x ".*" "*/.*"
+zip -r "$OUTPUT_FILE" . -x ".*" "*/.*" "*.sh"
 
 echo "Archivio creato con successo: $OUTPUT_FILE"
